bb26d80a0bfbe7245e0c272333b68f17c12f371b
lhidayatullah <54337604+lhidayatullah@users.noreply.github.com>
2023-10-26 20:13:17 +0700

    Add files via upload

---
+++ 1301224158_Lukman.py
@@ -0,0 +1,7 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+student_id = "1301224158"
+full_name = "Lukman Alfaris Hidayatullah"
+
+print("Student ID : ", student_id)
+print("Full Name : ", full_name)
